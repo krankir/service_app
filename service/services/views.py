2e055813c3293f1c26391a4f83f12f8e235c0597
Анатолий Редько <stroitel.proekt@gmail.com>
2023-02-12 15:35:45 +0500

    add manual cache

--- service/services/views.py
+++ service/services/views.py
@@ -1,7 +1,9 @@
-from django.db.models import Prefetch, F, Sum
+from django.db.models import Prefetch, Sum
+from django.core.cache import cache
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import Client
+from django.conf import settings
 from services.models import Subscription
 from services.serializers import SubscriptionSerializer
 
@@ -20,8 +22,16 @@ class SubscriptionView(ReadOnlyModelViewSet):
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
         response = super().list(request, *args, **kwargs)
+
+        price_cache = cache.get(settings.PRICE_CACHE_NAME)
+
+        if price_cache:
+            total_price = price_cache
+        else:
+            total_price = queryset.aggregate(total=Sum('price')).get('total')
+            cache.set(settings.PRICE_CACHE_NAME, total_price, 60 * 60)
+
         response_data = {'result': response.data,
-                         'total_amount': queryset.aggregate(
-                             total=Sum('price')).get('total')}
+                         'total_amount': total_price}
         response.data = response_data
         return response
